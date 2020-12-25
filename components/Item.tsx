799407ffe107f679d6d33f3531007e790a64e10f
admin <admin@admins-MacBook-Pro.local>
2020-12-25 13:58:12 +0200

    make notes animated

--- components/Item.tsx
+++ components/Item.tsx
@@ -1,5 +1,5 @@
-import React, { useMemo } from 'react';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
+import { Animated, Easing, Pressable, StyleSheet, Text, View } from 'react-native';
 import { Font } from '../constants';
 import { ColorTheme, ListItem, RootState } from '../types';
 import { wp } from '../utils';
@@ -11,35 +11,94 @@ interface Props extends ListItem {
   [props: string]: any,
 }
 
-function Item({ id, title, isDone, style, colors, ...props }: Props) {
+function Item({ id, title, isDone, note, isDaily, style, colors, onPress, ...props }: Props) {
   const styles = useMemo(() => getStyles(colors), [colors]);
+  const [isOpen, setOpenFlag] = useState(false);
+  const [noteMaxHeight, setNoteHeight] = useState(150);
+  const [itemHeight, setItemHeight] = useState(50);
+  const animated = useRef(new Animated.Value(0)).current;
+
+  const runAnimation = (targetValue: number) => {
+    Animated.timing(
+      animated,
+      {
+        duration: 400,
+        toValue: targetValue,
+        easing: Easing.bezier(.43, 0, .55, 1),
+        useNativeDriver: false,
+      }
+    ).start();
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      runAnimation(1);
+    } else {
+      runAnimation(0);
+    }
+  }, [isOpen])
+
+  const containerHeight = animated.interpolate({
+    inputRange: [0, 1],
+    outputRange: [itemHeight, noteMaxHeight + itemHeight]
+  })
 
   return (
-    <Pressable {...props} style={[styles.item, style]}>
-      <View style={[
-        styles.checkbox,
+    <Animated.View {...props} style={[
+      styles.container, style,
+      { height: containerHeight }
+    ]}>
+      <View style={styles.item} onLayout={({ nativeEvent }) => {
+        setItemHeight(nativeEvent.layout.height);
+      }}>
+
+        <Pressable style={{ flex: 1, ...styles.item }} onPress={onPress}>
+          <View style={[
+            styles.checkbox,
+            {
+              backgroundColor: isDone ? colors.primary : colors.background,
+              borderColor: isDone ? colors.primary : colors.border,
+            }
+          ]}>
+            {isDone && <Icon name='done' size={wp(24)} color={'#f5f5f5'} />}
+          </View>
+
+          <Text style={styles.title}>{title}</Text>
+
+          {
+            isDaily && (
+              <View>
+                <Icon name='date-range' size={wp(28)} color={colors.border} />
+                <View style={styles.dailyBadge} />
+              </View>
+            )
+          }
+        </Pressable>
+
         {
-          backgroundColor: isDone ? colors.primary : colors.background,
-          borderColor: isDone ? colors.primary : colors.border,
-        }
-      ]}>
-        {
-          isDone && (
-            <Icon
-              name='done'
-              size={wp(24)}
-              color={'#f5f5f5'}
-              style={{ textAlign: 'center' }} />
-          )
+          note ? (
+            <Pressable style={{ paddingLeft: wp(20), }} onPress={() => setOpenFlag(!isOpen)}>
+              <Icon name={'expand-more'} size={wp(32)} color={colors.border} />
+            </Pressable>
+          ) : null
         }
       </View>
 
-      <Text style={styles.title}>{title}</Text>
-    </Pressable>
+      <Text
+        onLayout={({ nativeEvent }) => setNoteHeight(nativeEvent.layout.height)}
+        style={[
+          { top: itemHeight },
+          styles.note
+        ]}>{note}</Text>
+    </Animated.View>
   )
 }
 
 const getStyles = (colors: ColorTheme) => StyleSheet.create({
+  container: {
+    overflow: 'hidden',
+    backgroundColor: colors.background
+  },
   item: {
     flexDirection: 'row',
     alignItems: 'center',
@@ -54,9 +113,29 @@ const getStyles = (colors: ColorTheme) => StyleSheet.create({
     borderWidth: 1,
   },
   title: {
+    flex: 1,
     fontSize: wp(16),
     color: colors.text,
     padding: 0,
+    fontFamily: Font.REGULAR
+  },
+  dailyBadge: {
+    position: 'absolute',
+    right: 0,
+    top: 0,
+    width: wp(8),
+    height: wp(8),
+    borderRadius: wp(8),
+    backgroundColor: colors.primary
+  },
+  note: {
+    position: 'absolute',
+    paddingLeft: wp(50),
+    paddingTop: wp(8),
+    fontSize: wp(16),
+    lineHeight: wp(24),
+    color: colors.text,
+    opacity: .7,
     fontFamily: Font.REGULAR
   }
 })
